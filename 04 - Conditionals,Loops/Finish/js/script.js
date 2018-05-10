986fce284fa5cf9c39d68765dfe7bfd2b7e7e783
Klim klimentov <klimentov@i.ua>
2018-05-10 12:10:24 +0300

    Did all the functions except nameUserVerification

--- 04 - Conditionals,Loops/Finish/js/script.js
+++ 04 - Conditionals,Loops/Finish/js/script.js
@@ -70,14 +70,16 @@ console.clear();
 // // Название месяца должно иметь вид строки
 // // Например = 14 Июня 2017
 //
+
+//
+// // * Your code Start *
 // var date = new Date();
 // var day = date.getDate(); // Текущий день
 // var month = date.getMonth(); // Текущий месяц (от 0 до 11)
 // var year = date.getFullYear(); // Текущий год
-// var monthArrey = ['Января','Февраля','Марта','Апреля','Мая','Июня','Июля','Августа','Сентября','Октября','Ноября','Декабря'];
+// var monthArray = ['Января','Февраля','Марта','Апреля','Мая','Июня','Июля','Августа','Сентября','Октября','Ноября','Декабря'];
 // var monthName = ''; // Переменная для значения
-//
-// // * Your code Start *
+
 // var i=0;
 // while (i<= 11){
 //
@@ -85,24 +87,23 @@ console.clear();
 //     i++;
 // }
 // switch (month) {
-//     case month=0:    monthName=monthArrey[0];    break;
-//     case month=1:    monthName=monthArrey[1];    break;
-//     case month=2:    monthName=monthArrey[2];    break;
-//     case month=3:    monthName=monthArrey[3];    break;
-//     case month=4:    monthName=monthArrey[4];    break;
-//     case month=5:    monthName=monthArrey[5];    break;
-//     case month=6:    monthName=monthArrey[6];    break;
-//     case month=7:    monthName=monthArrey[7];    break;
-//     case month=8:    monthName=monthArrey[8];    break;
-//     case month=9:    monthName=monthArrey[9];    break;
-//     case month=10:    monthName=monthArrey[10];    break;
-//     case month=11:    monthName=monthArrey[11];    break;
-//
+//     case month=0:    monthName=monthArraay[0];    break;
+//     case month=1:    monthName=monthArray[1];    break;
+//     case month=2:    monthName=monthArray[2];    break;
+//     case month=3:    monthName=monthArray[3];    break;
+//     case month=4:    monthName=monthArray[4];    break;
+//     case month=5:    monthName=monthArray[5];    break;
+//     case month=6:    monthName=monthArray[6];    break;
+//     case month=7:    monthName=monthArray[7];    break;
+//     case month=8:    monthName=monthArray[8];    break;
+//     case month=9:    monthName=monthArray[9];    break;
+//     case month=10:   monthName=monthArray[10];    break;
+//     case month=11:   monthName=monthArray[11];    break;
 // }
-// // * Your code End *
-//
 // console.log(`Today is: ${day} ${monthName} ${year} года`) // Пример: 14 Июня 2017
-//
+// * Your code End *
+
+
 //
 //
 // // ## 3 Взаимодействие с пользователем
@@ -114,20 +115,38 @@ console.clear();
 // // -- Если пароль совпадает - вывести сообщение о том что пароль верный и прервите цикл
 //
 // var users = [{
-//   name: 'Alex',
-//   password: 123
+//     name: 'Alex',
+//     password: 123
 // }, {
-//   name: 'Chris',
-//   password: 456
+//     name: 'Chris',
+//     password: 456
 // }, {
-//   name: 'Bill',
-//   password: 789
+//     name: 'Bill',
+//     password: 789
 // }];
 //
 // // * Your code Start *
+// console.log(users.name);
+// var nameUserVerification = prompt('Enter you name: ', 'Mishovur');
+// var nameUserPasswordVerification ='';
 //
+// var use6rVerification = function () {
+//         users.filter(function (item) {
+//             {
+//                 return item.name==nameUserPasswordVerification;
+//             }
 //
-//
+//         })
+//     };
+// console.log(use6rVerification());
+
+// users.forEach(function(element) {
+//     console.log(element);
+//     console.log(element.name);
+//     console.log(element.password);
+// });
+//userVerification(nameUser);
+
 // // * Your code End *
 //
 //
@@ -136,22 +155,35 @@ console.clear();
 // // Цикл прирывается только в том случае если пользователь ввел что либо кроме числа
 // // После прерывания цикла выводится сумма введеных чисел
 //
-// var sumArray = prompt('Please enter number', 0);
+// var sumArray = +prompt('Please enter number', 0);
+// var sum = 4;
 //
-// // * Your code Start *
+// function summma(sumArray) {
+//     do {
+//         sum = sumArray + sum;
+//         sumArray = +prompt('Please enter number', 0);
+//     }
+//     while (isFinite(sumArray)) ;
+//     alert('The sum of your number: '+ sum);
+// }
 //
-//
+// summma(sumArray);
+
 // // * Your code End *
 //
 // // ## 5
 // // Найдите сумму  1 + 2 + 3 + ... + N, где число N вводится пользователем из prompt
 //
-// var summetNumber = prompt('Please enter number', 0);
+
 //
 // // * Your code Start *
-//
-//
-//
+
+// var summetNumber = +prompt('Please enter number', 0);
+// function sumArithmeticProgression(summetNumber) {
+//     sum=(1+summetNumber)*(summetNumber)/2;
+//     alert(sum);
+// }
+// sumArithmeticProgression(summetNumber);
 // // * Your code End *
 //
 //
@@ -163,16 +195,30 @@ console.clear();
 // // Например что бы нарисовать 1 квадрат нужно вызвать ее 1 раз: drawFraction();
 // // Так же доски должны иметь черный\белый цвет. Реализовать это можно при помощи как css так и js
 //
-// var drawFraction = function(){
-//   const board = document.querySelector('.board');
-//   const fraction = document.createElement('div');
-//   board.appendChild(fraction);
+
+// // * Your code Start *
+// var color = "";
+// const size = 8;
+// var drawFraction = function (color) {
+//     const board = document.querySelector('.board');
+//     const fraction = document.createElement('div');
+//     fraction.style.background = color;
+//     board.appendChild(fraction);
+//
+//
 // }
 //
-// const size = 8;
-// drawFraction()
-// // * Your code Start *
 //
-//
-//
+// for (var i = 0; i < size; i += 1) {
+//     for (var j = 0; j < size; j += 1) {
+//         if ((i + j) % 2 == 0) {
+//             drawFraction("white");
+//             // console.log(color +'i:='+i +'j:='+j);
+//         }
+//         else {
+//             drawFraction("black");
+//             // console.log(color +'i:='+i +'j:='+j);
+//         }
+//     }
+// }
 // // * Your code End *
